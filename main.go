475fc8aad5edb7723b9c4258b51ccbcd3ca99784
Ana Clara <102690664+bax7os@users.noreply.github.com>
2025-06-02 15:13:28 -0400

    .

--- main.go
+++ main.go
@@ -11,7 +11,11 @@ import (
 func main() {
 	config.Carregar()
 	r := router.Gerar()
+	fs := http.FileServer(http.Dir("./uploads/fotos-perfil"))
+	http.Handle("/fotos/", http.StripPrefix("/fotos/", fs))
 
+	// Configuração para servir outros arquivos estáticos (CSS, JS, imagens padrão)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 }
